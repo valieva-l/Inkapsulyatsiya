5dd87432fde122c73f58d888b6466f6778c2dd65
valieva-l <l28075339@gmail.com>
2025-05-07 20:58:59 +0300

    Inkapsulyatsiya

---
+++ src/org/skypro/skyshop/basket/ProductBasket.java
@@ -0,0 +1,67 @@
+package org.skypro.skyshop.basket;
+
+import org.skypro.skyshop.product.Product;
+
+public class ProductBasket {
+    private final Product[] products = new Product[5];
+
+    // Метод добавления продукта
+    public void addProduct(Product product) {
+        for (int i = 0; i < products.length; i++) {
+            if (products[i] == null) {
+                products[i] = product;
+                return;
+            }
+        }
+
+        System.out.println("Невозможно добавить продукт");
+    }
+
+    // Метод получения общей стоимости
+    public int getTotalCost() {
+        int total = 0;
+        for (Product p : products) {
+            if (p != null) {
+                total += p.getProductСost();
+            }
+        }
+        return total;
+    }
+
+    // Метод печати содержимого
+    public void printContents() {
+        boolean empty = true;
+        for (Product p : products) {
+            if (p != null) {
+                System.out.println(p.getProductName() + ": " + p.getProductСost());
+                empty = false;
+            }
+        }
+        if (empty) {
+            System.out.println("в корзине пусто");
+        } else {
+            System.out.println("Итого: " + getTotalCost());
+        }
+    }
+
+    //метод поиска товара
+    public boolean containsProduct(Product product) {
+        for (Product p : products) {
+            if (p != null && p.equals(product)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Очистка корзины
+    public void clear() {
+        for (int i = 0; i < products.length; i++) {
+            products[i] = null;
+        }
+        System.out.println("Корзина очищена");
+    }
+
+}
+
+
---
+++ src/org/skypro/skyshop/product/Product.java
@@ -0,0 +1,32 @@
+package org.skypro.skyshop.product;
+
+public class Product {
+    private String productName;
+    private int productСost;
+
+
+    public Product(String productName, int productСost) {
+        this.productName = productName;
+        this.productСost = productСost;
+    }
+
+    public String getProductName() {
+
+        return productName;
+    }
+
+    public int getProductСost() {
+
+        return productСost;
+    }
+
+
+    // Готовим equals для сравнения товаров по имени (можешь расширять)
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Product)) return false;
+        Product other = (Product) obj;
+        return this.productName.equals(other.productName);
+    }
+}
