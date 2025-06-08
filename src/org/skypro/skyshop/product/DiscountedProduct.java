23fe03be5b5f09e2c2cdd460cdf23b4eb16d175b
valieva-l <l28075339@gmail.com>
2025-06-08 18:46:57 +0300

    Inkapsulyatsiya

--- src/org/skypro/skyshop/product/DiscountedProduct.java
+++ src/org/skypro/skyshop/product/DiscountedProduct.java
@@ -6,17 +6,16 @@ public class DiscountedProduct extends Product {
     private int discountPercentage;
 
     public DiscountedProduct(String productName, int basePrice, int discountPercentage) {
-        super(productName,basePrice);
+        super(productName, basePrice);
+        this.discountPercentage = discountPercentage;
         if (discountPercentage < 0 || discountPercentage > 100) {
             throw new IllegalArgumentException("Процент скидки должен быть в диапазоне от 0 до 100 включительно.");
         }
-        this.discountPercentage = discountPercentage;
     }
-
     @Override
     public int getProductСost() {
-        int discountAmount = (basePrice * discountPercentage) / 100; // Считаем сумму скидки
-        return basePrice - discountAmount; // Возвращаем цену со скидкой
+        int discountAmount = (basePrice * discountPercentage) / 100;
+        return basePrice - discountAmount;
     }
 
     public boolean getIsSpecial() {
@@ -33,6 +32,3 @@ public class DiscountedProduct extends Product {
         return super.equals(obj);
     }
 }
-
-
-
