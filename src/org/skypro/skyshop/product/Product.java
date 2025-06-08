4824d74a3d1645f59c897137f64cbcef3d289c2a
valieva-l <l28075339@gmail.com>
2025-06-08 16:54:37 +0300

    Inkapsulyatsiya

--- src/org/skypro/skyshop/product/Product.java
+++ src/org/skypro/skyshop/product/Product.java
@@ -1,47 +1,47 @@
 package org.skypro.skyshop.product;
 
-import org.skypro.skyshop.article.Searchable;
 
-public class Product implements Searchable {
-    private String productName;
-    private int productСost;
+public class Product {
 
-    public Product(String productName, int productСost) {
-        this.productName = productName;
-        this.productСost = productСost;
+        private String productName;
+        private int productСost;
+
+        public Product(String productName, int productСost) {
+            this.productName = productName;
+            this.productСost = productСost;
+        }
+
+        public String getProductName() {
+            return productName;
+        }
+
+        public int getProductСost() {
+            return productСost;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (!(obj instanceof Product)) return false;
+            Product other = (Product) obj;
+            return this.productName.equals(other.productName);
+        }
+
+
+        @Override
+        public String getSearchTerm() {
+            return productName;
+        }
+
+        @Override
+        public String getContentType() {
+            return "PRODUCT";
+
+        }
+
+        @Override
+        public String getSearchableName() {
+            return productName;
+        }
+
     }
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public int getProductСost() {
-        return productСost;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (!(obj instanceof Product)) return false;
-        Product other = (Product) obj;
-        return this.productName.equals(other.productName);
-    }
-
-
-    @Override
-    public String getSearchTerm() {
-        return productName;
-    }
-
-    @Override
-    public String getContentType() {
-        return "PRODUCT";
-
-    }
-
-    @Override
-    public String getSearchableName() {
-        return productName;
-    }
-
-}
