0613b41189e39125a75c39ab1991683cd706c428
valieva-l <l28075339@gmail.com>
2025-06-08 17:12:17 +0300

    Inkapsulyatsiya

--- src/org/skypro/skyshop/product/Product.java
+++ src/org/skypro/skyshop/product/Product.java
@@ -1,47 +1,32 @@
 package org.skypro.skyshop.product;
 
-
 public class Product {
-
-        private String productName;
-        private int productСost;
-
-        public Product(String productName, int productСost) {
-            this.productName = productName;
-            this.productСost = productСost;
-        }
-
-        public String getProductName() {
-            return productName;
-        }
-
-        public int getProductСost() {
-            return productСost;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj) return true;
-            if (!(obj instanceof Product)) return false;
-            Product other = (Product) obj;
-            return this.productName.equals(other.productName);
-        }
+    private String productName;
+    private int productСost;
 
 
-        @Override
-        public String getSearchTerm() {
-            return productName;
-        }
-
-        @Override
-        public String getContentType() {
-            return "PRODUCT";
-
-        }
-
-        @Override
-        public String getSearchableName() {
-            return productName;
-        }
-
+    public Product(String productName, int productСost) {
+        this.productName = productName;
+        this.productСost = productСost;
     }
+
+    public String getProductName() {
+
+        return productName;
+    }
+
+
+
+    public int getProductСost() {
+
+        return productСost;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Product)) return false;
+        Product other = (Product) obj;
+        return this.productName.equals(other.productName);
+    }
+}
